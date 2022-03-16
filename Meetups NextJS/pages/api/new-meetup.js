3d39ed416cac8522d9a36176fe79ce5c24a574f0
sharanuspatil <sharnup476@gmail.com>
2022-03-16 18:35:08 +0530

    Nextjs Project

---
+++ Meetups NextJS/pages/api/new-meetup.js
@@ -0,0 +1,27 @@
+import { MongoClient } from "mongodb";
+
+// /api/new-meetup
+// POST /api/new-meetup
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://Sharanu:Patil@123@cluster0.yp3rc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
+  }
+}
+
+export default handler;
