e09bbb8a2b50f42ab0bf1bf85eb0957bcef06de6
sharanuspatil <sharnup476@gmail.com>
2022-03-25 21:38:43 +0530

    Chat Application

---
+++ Chat Application/src/firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: " ",
+  authDomain: "",
+  projectId: "",
+  databaseURL: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { auth, db, storage };
